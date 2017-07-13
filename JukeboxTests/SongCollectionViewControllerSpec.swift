48ceab72150e8ea452f0d68f46fbcdf7ddfb1ccb
SangMee Specht <spechtse@miamioh.edu>
2017-07-13 14:29:02 -0500

    [Problem Set] add songCollectionViewController test

---
+++ JukeboxTests/SongCollectionViewControllerSpec.swift
@@ -0,0 +1,9 @@
+//
+//  SongCollectionViewControllerSpec.swift
+//  Jukebox
+//
+//  Created by SangMee Specht on 7/13/17.
+//  Copyright © 2017 SangMee Specht. All rights reserved.
+//
+
+import Foundation
